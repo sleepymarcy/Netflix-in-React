9a3e42e173a1a9d178ad24d99cd944ce73818ac6
Martyna <martyna.sowinska@hotmail.com>
2021-09-01 15:48:45 +0200

    Comment section and Details
    After clicking on a movie poster pops up a 'About the movie' details section, and comments

--- src/App.js
+++ src/App.js
@@ -17,3 +17,13 @@ function App() {
 }
 
 export default App
+
+// Create a Route for a ShowDetail component. It should be able to receive a ID parameter from the querystring.
+
+// Create the ShowDetail component that receives from the url the ID parameter and fetches all the informations
+// about the movie (from omdb & comments too).
+
+// Make every Show (Movie or Series) clickable and create an onClick redirect to the ShowDetail route.
+
+// From the main component pass also a prop called 'title' to every other component wrapped inside a Route,
+// and use it for displaying inside of it the title of that section (in an "h1").
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React from "react"
+import Comments from "./Comments"
 // import { Col,Card,Button } from "react-bootstrap";
 class MovieCard extends React.Component {
+  state = {
+    showCom: false,
+  };
+  // set to show
+  setShow() {
+    this.setState({ showCom: true })
+  }
+
+  handleCallback = (childData) => {
+    this.setState({ showCom: childData })
+  };
+
   render() {
-      console.log(this.props.movie)
     return (
-      <a className="col-2 my-3" key={this.props.movieId}>
-          <div className='movieCard h-100'>
-              <img className='w-100' src={this.props.img}/>
-            </div>
-      </a>
-    );
+      <>
+        <a
+          className="col-2 my-3"
+          key={this.props.movieId}
+          value={this.props.movieId}
+          onClick={() => {
+            this.setState({ showCom: true })
+          }}
+        >
+          <div className="movieCard h-100">
+            <img className="w-100" src={this.props.img} />
+          </div>
+        </a>
+        {this.state.showCom ? (
+          <Comments
+            parentCallback={this.handleCallback}
+            showit={this.state.showCom}
+            movId={this.props.movieId}
+          />
+        ) : (
+          <></>
+        )}
+      </>
+    )
   }
 }
 
-export default MovieCard;
+export default MovieCard
